bfd7090c7158dbb98422ac84582d66371bab36ac
Shykuzo <Stroobants.Lucas@hotmail.com>
2022-12-06 10:36:18 +0100

    [+] Log Class | Created 'logConsole(Level, String)' Method

--- src/fr/shykuzo/punishment/utilities/Log.java
+++ src/fr/shykuzo/punishment/utilities/Log.java
@@ -1,5 +1,22 @@
 package fr.shykuzo.punishment.utilities;
 
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
+
 public class Log {
 
+	public void consoleLog(Level severity, String message) {
+		Bukkit.getLogger().log(
+				severity,
+				String.format(
+						"§8----------------------------------------"
+						+ "\n\n" + "§f%s"
+						+ "\n\n" + "§8----------------------------------------",
+						
+						message
+				)
+		);
+	}
+	
 }
