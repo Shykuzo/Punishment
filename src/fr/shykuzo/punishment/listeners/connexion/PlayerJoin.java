938e7f181aa6466584580d9ccdeb2086dd279727
Shykuzo <Stroobants.Lucas@hotmail.com>
2022-12-07 08:36:25 +0100

    [+] PlayerJoin Class | Added Method

--- src/fr/shykuzo/punishment/listeners/connexion/PlayerJoin.java
+++ src/fr/shykuzo/punishment/listeners/connexion/PlayerJoin.java
@@ -1,5 +1,24 @@
 package fr.shykuzo.punishment.listeners.connexion;
 
-public class PlayerJoin {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
+import fr.shykuzo.punishment.Main;
+
+public class PlayerJoin implements Listener {
+
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		if(Main.getInstance().getConfigManager().isDatabaseModule() && Main.getInstance().getDatabaseManager().isConnected()) {
+			if(Main.getInstance().getPlayerManager().exists(event.getPlayer())) {
+				if(Main.getInstance().getConfigManager().isPremiumModule()) {
+					Main.getInstance().getPlayerManager().update(event.getPlayer());
+				}
+			} else {
+				Main.getInstance().getPlayerManager().insert(event.getPlayer());
+			}
+		}
+	}
+	
 }
