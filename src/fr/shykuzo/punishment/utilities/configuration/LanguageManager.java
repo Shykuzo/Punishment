a2c883379ed22166c9623bbdd788e693a32e72ad
Shykuzo <Stroobants.Lucas@hotmail.com>
2022-12-31 20:58:51 +0100

    [+] LanguageManager Class | Added Methods to get new Values

--- src/fr/shykuzo/punishment/utilities/configuration/LanguageManager.java
+++ src/fr/shykuzo/punishment/utilities/configuration/LanguageManager.java
@@ -3,8 +3,12 @@ package fr.shykuzo.punishment.utilities.configuration;
 import fr.shykuzo.punishment.Main;
 
 public class LanguageManager {
-
-		// ---------------------------------------- \\
+	
+	public String getString(String PATH) {
+		return getMessage(Main.getInstance().getLanguageConfig().getString(PATH));
+	}
+	
+				// -------------------- \\
 	
 	public String getMessage(String value) {
 		String message = value;
@@ -18,6 +22,17 @@ public class LanguageManager {
 	
 				// -------------------- \\
 	
+	public String getBroadcastMessage(String PATH, String PLAYER, String MODERATOR, String DURATION, String REASON) {
+		String value = getString("PUNISHMENT.BROADCAST." + PATH);
+		
+		if(value.contains("{PLAYER}")) value = value.replace("{PLAYER}", PLAYER);
+		if(value.contains("{MODERATOR}")) value = value.replace("{MODERATOR}", MODERATOR);
+		if(value.contains("{DURATION}")) value = value.replace("{DURATION}", DURATION);
+		if(value.contains("{REASON}")) value = value.replace("{REASON}", REASON);
+		
+		return value;
+	}
+	
 	public String getPunishmentMessage(String PATH, String MODERATOR, String REASON, String REMAINING_TIME, String DATE) {
 		StringBuilder stringBuilder = new StringBuilder();
 		
@@ -27,6 +42,16 @@ public class LanguageManager {
 			if(value.contains("{REMAINING_TIME}")) value = value.replace("{REMAINING_TIME}", REMAINING_TIME);
 			if(value.contains("{DATE}")) value = value.replace("{DATE}", DATE);
 			
+			stringBuilder.append(getMessage(value) + "\n");
+		}
+		
+		return stringBuilder.toString();
+	}
+	
+	public String getHelpMessage(String PATH) {
+		StringBuilder stringBuilder = new StringBuilder();
+		
+		for(String value : Main.getInstance().getLanguageConfig().getStringList("HELP." + PATH)) {
 			stringBuilder.append(getMessage(value) + "\n");
 		}
 		
