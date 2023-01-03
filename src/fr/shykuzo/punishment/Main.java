ec74cb64aee3bae321f3391f0ba7ca5d95dc7e96
Shykuzo <Stroobants.Lucas@hotmail.com>
2023-01-03 12:09:42 +0100

    [~] Main Class & Log Class | Fixed Message

--- src/fr/shykuzo/punishment/Main.java
+++ src/fr/shykuzo/punishment/Main.java
@@ -73,7 +73,7 @@ public class Main extends JavaPlugin {
 			new Log().consoleLog(
 					Level.WARNING,
 					String.format(
-							"\n" + "The 'Discord' module could not be activated !"
+							"The 'Discord' module could not be activated !"
 							+ "\n" + "Status : %s",
 							
 							(getConfigManager().isDatabaseModule() ? "Error (Missing Token)" : "Normal (Module Disabled)")
