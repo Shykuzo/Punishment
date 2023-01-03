ec6e6136912f28167a0eec6c7b29534c8f0bfadc
Shykuzo <Stroobants.Lucas@hotmail.com>
2023-01-03 14:05:17 +0100

    [+] Main Class & Plugin Configuration | Registered 'UNBAN' command

--- src/fr/shykuzo/punishment/Main.java
+++ src/fr/shykuzo/punishment/Main.java
@@ -9,6 +9,7 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.shykuzo.punishment.commands.moderation.ban.Ban;
+import fr.shykuzo.punishment.commands.moderation.ban.UnBan;
 import fr.shykuzo.punishment.listeners.connexion.PlayerJoin;
 import fr.shykuzo.punishment.listeners.connexion.PlayerLogin;
 import fr.shykuzo.punishment.utilities.Log;
@@ -95,6 +96,7 @@ public class Main extends JavaPlugin {
 		Bukkit.getPluginManager().registerEvents(new PlayerLogin(), getInstance());
 		
 		getCommand("ban").setExecutor(new Ban());
+		getCommand("unban").setExecutor(new UnBan());
 	}
 	
 	@Override
