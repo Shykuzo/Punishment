dcc4b6132639ab5dc450b12b06caea187e3ab834
Shykuzo <Stroobants.Lucas@hotmail.com>
2023-01-03 20:11:10 +0100

    [~] Log Class | Fixed Color

--- src/fr/shykuzo/punishment/utilities/Log.java
+++ src/fr/shykuzo/punishment/utilities/Log.java
@@ -7,16 +7,27 @@ import org.bukkit.Bukkit;
 public class Log {
 
 	public void consoleLog(Level severity, String message) {
-		Bukkit.getLogger().log(
-				severity,
+		Bukkit.getConsoleSender().sendMessage(
 				String.format(
 						"\n" + "§8----------------------------------------"
-						+ "\n\n" + "§f%s"
+						+ "\n\n" + "%s%s"
 						+ "\n\n" + "§8----------------------------------------",
 						
+						setColor(severity),
 						message
 				)
 		);
+	}
+	
+		// -------------------- \\
+	
+	private String setColor(Level severity) {
+		if(severity == Level.INFO) return "§f";
+		if(severity == Level.FINE) return "§a";
+		if(severity == Level.WARNING) return "§6";
+		if(severity == Level.SEVERE) return "§c";
+		
+		return "§f";
 	}
 	
 }
